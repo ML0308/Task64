17ec9d6676ad21c21276b0e71683cde0e18e1138
Максим Лесков <9185960@mail.ru>
2022-12-26 18:12:22 +0700

    Task commit

---
+++ Task64/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Task64
+{
+    class Program
+    {
+        public static void NaturalNumbers(int n)
+        {
+            if (n > 0)
+            {
+                Console.Write("{0} ", n);
+                NaturalNumbers(n - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            NaturalNumbers(8);
+            Console.Read();
+        }
+    }
+}
